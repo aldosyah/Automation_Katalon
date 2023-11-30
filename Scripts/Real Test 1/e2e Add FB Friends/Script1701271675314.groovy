7182d40330912acea78804bd43a20165993e3558
user <user@DESKTOP-8LUHHTT>
2023-11-30 12:49:57 +0700

    real test

--- Scripts/Real Test 1/e2e Add FB Friends/Script1701271675314.groovy
+++ Scripts/Real Test 1/e2e Add FB Friends/Script1701271675314.groovy
@@ -1,19 +1,20 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import static common.CO.cari
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.facebook.com')
+
+WebUI.setText(cari('//input[@id="email"]'), '081275265014')
+
+WebUI.setText(cari('//input[@type="password"]'), 'cvmuda200721')
+
+WebUI.click(cari('//button[@name="login"]'))
+
+WebUI.click(cari('//input[@placeholder="Cari di Facebook"]'))
+
+WebUI.setText(cari('//input[@placeholder="Cari di Facebook"]'), 'Alisha Syazani')
+
+WebUI.click(cari('(//div[@role="none"])[2]'))
+
+WebUI.click(cari("(//span[text()='Tambah teman'])[1]"))
